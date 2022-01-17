8ff3999f89c5c887c5eec7de2e734156fb2bc8ed
Ustenko Volodymyr <koldon6@gmail.com>
2022-01-17 20:02:32 +0200

    Fixing styles of button

--- src/pages/home/components/Toolbar/CartButton.jsx
+++ src/pages/home/components/Toolbar/CartButton.jsx
@@ -12,10 +12,10 @@ export default function CartButton() {
     return(
         <div className='cart-button-container'>
             <p>{goods[0].totalPrice}&#8372;</p>
+
             <span></span>
 
-            <div style={{display: 'flex', alignItems:'center'}}>
-
+            <div>
                 <svg className="cartIco" width="20" height="20" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.71374 25.9355C11.0385 25.9355 12.1124 24.8163 12.1124 23.4355C12.1124 22.0548 11.0385 20.9355 9.71374 20.9355C8.38901 20.9355 7.31509 22.0548 7.31509 23.4355C7.31509 24.8163 8.38901 25.9355 9.71374 25.9355Z" stroke="#558881" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M21.2273 25.9355C22.552 25.9355 23.6259 24.8163 23.6259 23.4355C23.6259 22.0548 22.552 20.9355 21.2273 20.9355C19.9025 20.9355 18.8286 22.0548 18.8286 23.4355C18.8286 24.8163 19.9025 25.9355 21.2273 25.9355Z" stroke="#558881" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
